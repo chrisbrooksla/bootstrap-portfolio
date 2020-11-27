768978c10d9ebbd1be542a31aeb35f8ed5f0f4f2
Chris Brooks <chrisbrooksla@gmail.com>
2020-11-27 14:02:59 -0600

    contact-form-good

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// contact form 
+
+$( '.js-input' ).keyup(function() {
+  if( $(this).val() ) {
+     $(this).addClass('not-empty');
+  } else {
+     $(this).removeClass('not-empty');
+  }
+});
+
+// end contact form
